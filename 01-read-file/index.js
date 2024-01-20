2ba59fbcf57150c0f4836229b17a84baa16d2757
Maryna S <rimana.ca@gmail.com>
2024-01-20 17:56:50 -0500

    refactor: remove default option utf for .createReadStream()

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,8 @@
-// const fs = require('fs');
-// const path = require('path');
-// const data = '';//in case, data is not provided
+// 1. Import the necessary modules for task execution:
+// 2. Create a new **ReadStream** from the file `text.txt`.
+// 3. Direct the read stream to the standard output stream.
 
-// const pathToTxt = path.join(__dirname, 'text.txt');
-// const rs = fs.createReadStream(pathToTxt, 'utf-8');
-
-//process.stdout.write() better than console.log() as c.l() might show unreadable chars
-//rs.on('data', (txt) => console.log(txt));
+//rs.on('data', (txt) => console.log(txt.toString());
 //rs.on('data', (txt) => process.stdout.write(txt));
 
 //fs.createReadStream(path, options)
@@ -14,8 +10,9 @@ const fs = require('fs');
 const path = require('path');
 
 const pathToTxt = path.join(__dirname, 'text.txt');
-const createReader = fs.createReadStream(pathToTxt, 'utf-8'); 
+//2.fs.createReadStream(path[, options])->to open up a file/stream to read data
+const rs = fs.createReadStream(pathToTxt);
 
-createReader.on('data', (data) => {
-  console.log(data.toString());
+rs.on('data', (txt) => {
+  console.log(txt.toString());
 });
