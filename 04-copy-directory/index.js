5834b9dfc335e9c209311ad629e1b024137e1b1f
Maryna S <rimana.ca@gmail.com>
2024-01-23 16:37:46 -0500

    fix: remove method - recursive - to rm all nested repositories-task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,7 +3,7 @@
 // 2. Create the `files-copy` folder if it does not exist yet.
 // 3. Read the contents of the `files` folder.
 // 4. Copy files from the `files` folder to the `files-copy` folder.
-const fs = require('fs/promises');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 const pathToFolder = path.join(__dirname, 'files');
@@ -11,19 +11,22 @@ const pathToDestFolder = path.join(__dirname, 'files-copy');
 
 async function copyDir() {
   try {
+    //fsPromises.rm(path[, options])-> to remove nested directories
+    //recursive -> set to true->  to remove nested directories!!!
+    await fsPromises.rm(pathToDestFolder, { recursive: true });
     //2. fsPromises.mkdir(path[, options])-to create dir
     //path-path of dir to be created
     //options-{ recursive: true }-> as otherwise, error if dir already exists
-    await fs.mkdir(pathToDestFolder, { recursive: true });
+    await fsPromises.mkdir(pathToDestFolder, { recursive: true });
     //3. fs.readdir(path)-read the contents of  dir
-    const files = await fs.readdir(pathToFolder);
+    const files = await fsPromises.readdir(pathToFolder);
 
     for (const x of files) {
       const src = path.join(pathToFolder, x);
       const dest = path.join(pathToDestFolder, x);
       //4. fsPromises.copyFile(src, dest[, mode])
       //dest is overwritten if it already exists
-      await fs.copyFile(src, dest);
+      await fsPromises.copyFile(src, dest);
     }
     console.log('The folded copied successfully.');
   } catch (error) {
